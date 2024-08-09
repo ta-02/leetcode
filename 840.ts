e2bce255f5d5c08d74c8439b59ee5ea80aa33a52
ta-02 <talal.gl2004@gmail.com>
2024-08-08 22:11:27 -0400

    updated 840

--- 840.ts
+++ 840.ts
@@ -1,52 +1,59 @@
 function numMagicSquaresInside(grid: number[][]): number {
   let count = 0;
-
-  for (let i = 0; i <= grid.length - 3; i++) {
-    for (let j = 0; j <= grid[0].length - 3; j++) {
-      const subgrid = [
-        [grid[i][j], grid[i][j + 1], grid[i][j + 2]],
-        [grid[i + 1][j], grid[i + 1][j + 1], grid[i + 1][j + 2]],
-        [grid[i + 2][j], grid[i + 2][j + 1], grid[i + 2][j + 2]],
-      ];
-      if (isMagicSquare(subgrid)) {
+  for (let i = 0; i < grid.length - 2; i++) {
+    for (let j = 0; j < grid[0].length - 2; j++) {
+      if (isMagicSquare(grid, i, j)) {
         count++;
       }
     }
   }
-
   return count;
 }
 
-function isMagicSquare(grid: number[][]): boolean {
-  const seenNumbers = new Set<number>();
+function isMagicSquare(
+  grid: number[][],
+  startR: number,
+  startC: number,
+): boolean {
+  const seenNums = new Set<number>();
+
+  const targetSum =
+    grid[startR][startC] + grid[startR][startC + 1] + grid[startR][startC + 2];
+
   for (let i = 0; i < 3; i++) {
+    let rowSum = 0;
     for (let j = 0; j < 3; j++) {
-      const num = grid[i][j];
-      if (num < 1 || num > 9 || seenNumbers.has(num)) {
+      const num = grid[startR + i][startC + j];
+      if (num < 1 || num > 9 || seenNums.has(num)) {
         return false;
       }
-      seenNumbers.add(num);
+      seenNums.add(num);
+      rowSum += num;
     }
-  }
-
-  const targetSum = grid[0][0] + grid[0][1] + grid[0][2];
-
-  for (let i = 0; i < 3; i++) {
-    const rowSum = grid[i][0] + grid[i][1] + grid[i][2];
     if (rowSum !== targetSum) {
       return false;
     }
   }
 
   for (let j = 0; j < 3; j++) {
-    const colSum = grid[0][j] + grid[1][j] + grid[2][j];
+    let colSum = 0;
+    for (let i = 0; i < 3; i++) {
+      colSum += grid[startR + i][startC + j];
+    }
     if (colSum !== targetSum) {
       return false;
     }
   }
 
-  const diagonal1Sum = grid[0][0] + grid[1][1] + grid[2][2];
-  const diagonal2Sum = grid[0][2] + grid[1][1] + grid[2][0];
+  const diagonal1Sum =
+    grid[startR][startC] +
+    grid[startR + 1][startC + 1] +
+    grid[startR + 2][startC + 2];
+  const diagonal2Sum =
+    grid[startR][startC + 2] +
+    grid[startR + 1][startC + 1] +
+    grid[startR + 2][startC];
+
   if (diagonal1Sum !== targetSum || diagonal2Sum !== targetSum) {
     return false;
   }
