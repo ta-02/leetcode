2ab5da0520183b74f39b7c746247d7d96ed6cc1d
ta-02 <talal.gl2004@gmail.com>
2024-08-21 20:35:16 -0400

    finsished 664

--- 664.py
+++ 664.py
@@ -1,4 +1,18 @@
+from functools import cache
+
+
 class Solution:
     def strangePrinter(self, s: str) -> int:
+        @cache
+        def dp(left, right):
+            if left > right:
+                return 0
+            if left == right:
+                return 1
+            best = 1 + dp(left + 1, right)
+            for mid in range(left + 1, right + 1):
+                if s[left] == s[mid]:
+                    best = min(best, dp(left, mid - 1) + dp(mid + 1, right))
+            return best
 
-        return 0
+        return dp(0, len(s) - 1)
