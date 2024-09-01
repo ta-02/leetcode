09806575925cec5994aaaed4aba4f787ea1dc423
ta-02 <talal.gl2004@gmail.com>
2024-08-31 21:43:59 -0400

    finsished 2022

---
+++ 2022.cpp
@@ -0,0 +1,23 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+        vector<vector<int>> res(m, vector<int>(n)); 
+
+        if (original.size() != m * n) { 
+            return {};
+        }
+        
+        int index = 0;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                res[i][j] = original[index++];
+            }
+        }
+
+        return res;
+    }
+};
