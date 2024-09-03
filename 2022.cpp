3844f25b3151d9c097b77f451b4bf03572f3820c
ta-02 <talal.gl2004@gmail.com>
2024-09-02 23:27:44 -0400

    finished 1945

--- 2022.cpp
+++ 2022.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 class Solution {
 public:
-    vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
-        vector<vector<int>> res(m, vector<int>(n)); 
+  vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+    vector<vector<int>> res(m, vector<int>(n));
 
-        if (original.size() != m * n) { 
-            return {};
-        }
-        
-        int index = 0;
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                res[i][j] = original[index++];
-            }
-        }
-
-        return res;
+    if (original.size() != m * n) {
+      return {};
     }
+
+    int index = 0;
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        res[i][j] = original[index++];
+      }
+    }
+
+    return res;
+  }
 };
