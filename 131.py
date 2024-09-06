a0d923dd06652cbe40b44ea504fab54f4a3548d4
ta-02 <talal.gl2004@gmail.com>
2024-09-06 17:30:31 -0400

    finished 70

--- 131.py
+++ 131.py
@@ -1,29 +1,18 @@
-from typing import List
-
-
 class Solution:
-    def partition(self, s: str) -> List[List[str]]:
-      res = []
-      path = []
-      N = len(s)
+    def partition(self, s: str) -> list[list[str]]:
+        res = []
+        path = []
+        N = len(s)
 
-      def dfs(i):
-        if i >= N:
-          res.append(path[:])
-          return  
-        for j in range(i, N):
-          if s[i:j+1] == s[i:j+1][::-1]:
-            path.append(s[i:j+1])
-            dfs(j+1)
-            path.pop()
-            
-      dfs(0)
-      return res
+        def dfs(i):
+            if i >= N:
+                res.append(path[:])
+                return
+            for j in range(i, N):
+                if s[i : j + 1] == s[i : j + 1][::-1]:
+                    path.append(s[i : j + 1])
+                    dfs(j + 1)
+                    path.pop()
 
-
-        
-            
-
-
-        
-            
+        dfs(0)
+        return res
