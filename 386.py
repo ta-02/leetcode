ba40ce829f717f666c4a87d86ef00fe22dff41b0
ta-02 <talal.gl2004@gmail.com>
2024-09-21 13:01:12 -0400

    finished 386

---
+++ 386.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        res = []
+        curr = 1
+
+        while len(res) < n:
+            res.append(curr)
+
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr == n or curr % 10 == 9:
+                    curr = curr // 10
+                curr += 1
+
+        return res
