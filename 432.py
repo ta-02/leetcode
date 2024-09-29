f916b5c52af8940d77ea2828bd1385c31eefbbaf
ta-02 <talal.gl2004@gmail.com>
2024-09-29 17:11:31 -0400

    finished 432

--- 432.py
+++ 432.py
@@ -1,45 +1,84 @@
-class AllOne:
+class Node:
+    def __init__(self, freq):
+        self.freq = freq
+        self.keys = set()
+        self.prev = None
+        self.next = None
 
+
+class AllOne:
     def __init__(self):
-        self.map = {}
-        self.max = ()
-        self.min = ()
+        self.head = Node(0)
+        self.tail = Node(0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.key_to_node = {}
+
+    def _add_node_after(self, new_node, prev_node):
+        next_node = prev_node.next
+        prev_node.next = new_node
+        new_node.prev = prev_node
+        new_node.next = next_node
+        next_node.prev = new_node
+
+    def _remove_node(self, node):
+        prev_node = node.prev
+        next_node = node.next
+        prev_node.next = next_node
+        next_node.prev = prev_node
 
     def inc(self, key: str) -> None:
-        if key in self.map:
-            self.map[key] += 1
+        if key in self.key_to_node:
+            node = self.key_to_node[key]
+            next_freq = node.freq + 1
+            node.keys.remove(key)
+            if node.next.freq != next_freq:
+                new_node = Node(next_freq)
+                new_node.keys.add(key)
+                self._add_node_after(new_node, node)
+            else:
+                node.next.keys.add(key)
+            self.key_to_node[key] = (
+                node.next if node.next.freq == next_freq else new_node
+            )
+            if not node.keys:
+                self._remove_node(node)
         else:
-            self.map[key] = 1
-
-        if self.max:
-            if self.map[key] > self.max[1]:
-                self.max = (key, self.map[key])
-        else:
-            self.max = (key, self.map[key])
+            if self.head.next.freq != 1:
+                new_node = Node(1)
+                new_node.keys.add(key)
+                self._add_node_after(new_node, self.head)
+            else:
+                self.head.next.keys.add(key)
+            self.key_to_node[key] = self.head.next
 
     def dec(self, key: str) -> None:
-        count = self.map[key]
-        if count - 1 == 0:
-            self.map.pop(key)
+        if key not in self.key_to_node:
+            return
+        node = self.key_to_node[key]
+        node.keys.remove(key)
+        if node.freq == 1:
+            del self.key_to_node[key]
         else:
-            self.map[key] = count - 1
+            prev_freq = node.freq - 1
+            if node.prev.freq != prev_freq:
+                new_node = Node(prev_freq)
+                new_node.keys.add(key)
+                self._add_node_after(new_node, node.prev)
+            else:
+                node.prev.keys.add(key)
+            self.key_to_node[key] = (
+                node.prev if node.prev.freq == prev_freq else new_node
+            )
+        if not node.keys:
+            self._remove_node(node)
 
     def getMaxKey(self) -> str:
-        if len(self.map) == 0:
+        if self.tail.prev == self.head:
             return ""
-
-        return max(self.map)
+        return next(iter(self.tail.prev.keys))
 
     def getMinKey(self) -> str:
-        if len(self.map) == 0:
+        if self.head.next == self.tail:
             return ""
-
-        return min(self.map)
-
-
-# Your AllOne object will be instantiated and called as such:
-# obj = AllOne()
-# obj.inc(key)
-# obj.dec(key)
-# param_3 = obj.getMaxKey()
-# param_4 = obj.getMinKey()
+        return next(iter(self.head.next.keys))
